e40c416d2e6a439ec9f034475e3e40c747f96a9c
Deepak Kumar <40654292+deepak-bits@users.noreply.github.com>
2023-07-15 20:56:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your JS code here. If required.
+const div = document.createElement('div');
+div.id = 'line';
+
+div.style.width = '200px';
+div.style.height = '2px';
+div.style.backgroundColor = 'black';
+document.body.appendChild(div);
+
+let i = 0;
+setInterval(function() {
+    // console.log(i);
+  i++;
+  div.style.transform = `rotate(${i%360}deg)`;
+}, 25);
